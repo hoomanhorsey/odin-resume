7897f5e16f233bc9f53f68a376a0890d43c7f450
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2025-04-03 12:41:41 +1100

    Created a filter to render h2 subheadings, and then map to render each element according to category

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,11 @@ import "./App.css";
 import { GlobalCV } from "./components/details";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  const firstName = "blah";
   return (
     <>
       <h1>Resume</h1>
-      {/* <FullName /> */}
-      <GlobalCV />
       <div>
-        {/* <Details /> */}
-        {/* <LastName /> */}
-        {/* <Resume firstName={firstName} /> */}
+        <GlobalCV />
       </div>
     </>
   );
--- src/components/details.jsx
+++ src/components/details.jsx
@@ -2,15 +2,69 @@ import { useState } from "react";
 
 function GlobalCV() {
   const defaultCV = [
-    { id: 1, label: "Name", value: "Jimmy", editStatus: false },
-    { id: 2, label: "Email", value: "jimmy@jimmy.com", editStatus: true },
-    { id: 3, label: "Phone", value: "123456789", editStatus: false },
-    { id: 4, label: "School", value: "#", editStatus: false },
-    { id: 5, label: "Course Details", value: "#", editStatus: false },
-    { id: 6, label: "Dates", value: "#", editStatus: false },
-    { id: 7, label: "Company Name", value: "#", editStatus: false },
-    { id: 8, label: "Position Title", value: "#", editStatus: false },
-    { id: 9, label: "Dates", value: "#", editStatus: false },
+    {
+      id: 1,
+      category: "Details",
+      label: "Name",
+      value: "Jimmy",
+      editStatus: false,
+    },
+    {
+      id: 2,
+      category: "Details",
+      label: "Email",
+      value: "jimmy@jimmy.com",
+      editStatus: true,
+    },
+    {
+      id: 3,
+      category: "Details",
+      label: "Phone",
+      value: "123456789",
+      editStatus: false,
+    },
+    {
+      id: 4,
+      category: "Education",
+      label: "School",
+      value: "University of Sydney",
+      editStatus: false,
+    },
+    {
+      id: 5,
+      category: "Education",
+      label: "Course Details",
+      value: "Bachelor of Law",
+      editStatus: false,
+    },
+    {
+      id: 6,
+      category: "Education",
+      label: "Dates",
+      value: "1997 - 1999",
+      editStatus: false,
+    },
+    {
+      id: 7,
+      category: "Work",
+      label: "Company Name",
+      value: "Big Law Firm",
+      editStatus: false,
+    },
+    {
+      id: 8,
+      category: "Work",
+      label: "Position Title",
+      value: "Chief Legal Person",
+      editStatus: false,
+    },
+    {
+      id: 9,
+      category: "Work",
+      label: "Dates",
+      value: "2000-2017",
+      editStatus: false,
+    },
   ];
 
   const [CVDetails, setCVDetails] = useState(defaultCV);
@@ -32,7 +86,6 @@ function GlobalCV() {
   };
 
   const submitInfo = (id) => {
-    // console.log(id, "submitted info", CVDetails[id].value);
     setCVDetails((prevState) =>
       prevState.map((item) =>
         item.id === id
@@ -45,25 +98,132 @@ function GlobalCV() {
     );
   };
 
-  const editStatusRender = CVDetails.map((item) =>
+  const categories = ["Details", "Education", "Work"];
+
+  return categories.map((category) => (
+    <div key={category}>
+      <h2>{category}</h2>
+      {CVDetails.filter((item) => item.category === category).map((item) =>
+        item.editStatus === false ? (
+          <div class="itemContainer" key={item.id}>
+            <div class="itemLabel">{item.label}</div>
+            <input
+              class="itemInput"
+              type="text"
+              value={item.value}
+              onChange={(e) => updateValue(item.id, e.target.value)}
+            ></input>
+
+            <div>
+              <button class="itemButton" onClick={() => submitInfo(item.id)}>
+                Submit
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div class="itemContainer" key={item.id}>
+            <div class="itemLabel">{item.label}:</div>
+            <div class="itemValue">{item.value}</div>{" "}
+            <button class="itemButton" onClick={() => editStatus(item.id)}>
+              Edit
+            </button>
+          </div>
+        )
+      )}
+
+      {/* {CVDetails.filter((item) => item.category === category).map((item) => (
+        <div key={item.id} item={item} />
+      ))} */}
+
+      {/* {CVDetails.map((item) =>
+        item.editStatus === false ? <div> Poo</div> : <div>NonPoo</div>
+      )} */}
+
+      {/* {CVDetails.map((item) =>
+        item.editStatus === false ? (
+          <div class="itemContainer" key={item.id}>
+            <div class="itemLabel">{item.label}</div>
+            <input
+              class="itemInput"
+              type="text"
+              value={item.value}
+              onChange={(e) => updateValue(item.id, e.target.value)}
+            ></input>
+
+            <div>
+              <button class="itemButton" onClick={() => submitInfo(item.id)}>
+                Submit
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div class="itemContainer" key={item.id}>
+            <div class="itemLabel">{item.label}:</div>
+            <div class="itemValue">{item.value}</div>{" "}
+            <button class="itemButton" onClick={() => editStatus(item.id)}>
+              Edit
+            </button>
+          </div>
+        )
+      )} */}
+    </div>
+  ));
+
+  CVDetails.map((item) =>
     item.editStatus === false ? (
-      <div key={item.id}>
-        {item.label}
+      <div class="itemContainer" key={item.id}>
+        <div class="itemLabel">{item.label}</div>
         <input
+          class="itemInput"
           type="text"
           value={item.value}
           onChange={(e) => updateValue(item.id, e.target.value)}
         ></input>
-        <button onClick={() => submitInfo(item.id)}>Submit</button>
+
+        <div>
+          <button class="itemButton" onClick={() => submitInfo(item.id)}>
+            Submit
+          </button>
+        </div>
       </div>
     ) : (
-      <div key={item.id}>
-        {item.label}: {item.value}{" "}
-        <button onClick={() => editStatus(item.id)}>Edit</button>
+      <div class="itemContainer" key={item.id}>
+        <div class="itemLabel">{item.label}:</div>
+        <div class="itemValue">{item.value}</div>{" "}
+        <button class="itemButton" onClick={() => editStatus(item.id)}>
+          Edit
+        </button>
       </div>
     )
   );
-  return editStatusRender;
+
+  // return CVDetails.map((item) =>
+  //   item.editStatus === false ? (
+  //     <div class="itemContainer" key={item.id}>
+  //       <div class="itemLabel">{item.label}</div>
+  //       <input
+  //         class="itemInput"
+  //         type="text"
+  //         value={item.value}
+  //         onChange={(e) => updateValue(item.id, e.target.value)}
+  //       ></input>
+
+  //       <div>
+  //         <button class="itemButton" onClick={() => submitInfo(item.id)}>
+  //           Submit
+  //         </button>
+  //       </div>
+  //     </div>
+  //   ) : (
+  //     <div class="itemContainer" key={item.id}>
+  //       <div class="itemLabel">{item.label}:</div>
+  //       <div class="itemValue">{item.value}</div>{" "}
+  //       <button class="itemButton" onClick={() => editStatus(item.id)}>
+  //         Edit
+  //       </button>
+  //     </div>
+  //   )
+  // );
 }
 
 export { GlobalCV };
